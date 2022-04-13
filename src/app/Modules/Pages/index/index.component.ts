a38d12edcbf0f4609cdc587c84641ee4f709d773
Farras MUF <farras.prabowo@muf.co.id>
2022-04-13 21:50:52 +0700

    Perbaikan format date dan sort table header

--- src/app/Modules/Pages/index/index.component.ts
+++ src/app/Modules/Pages/index/index.component.ts
@@ -1,6 +1,7 @@
 import { EmployeeService } from 'src/app/_services/employee.service';
 import { Employee } from './../../../Models/Employee';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Sort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-index',
@@ -30,6 +31,39 @@ export class IndexComponent implements OnInit {
     )
   }
 
+  sortData(sort: Sort) {
+    const data = this.posts.slice();
+    if (!sort.active || sort.direction === '') {
+      this.posts = data;
+      return;
+    }
+    this.posts = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'username':
+          return compare(a.username, b.username, isAsc);
+        case 'firstname':
+          return compare(a.firstname, b.firstname, isAsc);
+        case 'lastname':
+          return compare(a.lastname, b.lastname, isAsc);
+        case 'email':
+          return compare(a.email, b.email, isAsc);
+        case 'birthdate':
+          return compare(a.birthdate, b.birthdate, isAsc);
+        case 'basic_salary':
+          return compare(a.basic_salary, b.basic_salary, isAsc);
+        case 'status':
+          return compare(a.status, b.status, isAsc);
+        case 'groups':
+          return compare(a.groups, b.groups, isAsc);
+        case 'description':
+          return compare(a.description, b.description, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
   search() {
     this.posts = this.posts.filter(res => {
         return res.username.toLowerCase().match(this.searchValue.toLowerCase());
@@ -56,4 +90,7 @@ export class IndexComponent implements OnInit {
     this.tableSize = event.target.value;
     this.page = 1;
   }
+}
+function compare(a: number | string | Date, b: number | string | Date, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
